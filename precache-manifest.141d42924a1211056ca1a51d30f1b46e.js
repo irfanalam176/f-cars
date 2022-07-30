1e281d45712faa3774de1831d3868774d4212916
irfanalam176 <109797455+irfanalam176@users.noreply.github.com>
2022-07-30 02:10:34 -0700

    Updates

---
+++ precache-manifest.141d42924a1211056ca1a51d30f1b46e.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = [
+  {
+    "revision": "bafe5e202f56f5d9ad8a",
+    "url": "/f-cars/static/css/main.7ec12b0e.chunk.css"
+  },
+  {
+    "revision": "bafe5e202f56f5d9ad8a",
+    "url": "/f-cars/static/js/main.606e1989.chunk.js"
+  },
+  {
+    "revision": "9ae1fe5e432a792a7149",
+    "url": "/f-cars/static/js/runtime~main.25a20e97.js"
+  },
+  {
+    "revision": "aeb43772ff37f8a9a929",
+    "url": "/f-cars/static/js/2.da69726e.chunk.js"
+  },
+  {
+    "revision": "00f62158032c23924eb7",
+    "url": "/f-cars/static/js/3.80abd554.chunk.js"
+  },
+  {
+    "revision": "9c1139b1dd8d6bddede477f340b4be0f",
+    "url": "/f-cars/static/media/corolla1.9c1139b1.jpg"
+  },
+  {
+    "revision": "0c114c96e0baf059daeaa3a967307812",
+    "url": "/f-cars/static/media/corolla2.0c114c96.jpg"
+  },
+  {
+    "revision": "f35232bc728e913a237b7b4ddbfdb2ec",
+    "url": "/f-cars/static/media/corolla3.f35232bc.jpg"
+  },
+  {
+    "revision": "f5e803b3a505e21cdd75006a97144f99",
+    "url": "/f-cars/static/media/corolla4.f5e803b3.jpg"
+  },
+  {
+    "revision": "c5b6d1012e929b2a32b9d444a205137a",
+    "url": "/f-cars/static/media/corolla6.c5b6d101.jpg"
+  },
+  {
+    "revision": "be5e161aabbc31ac474885c915a903c3",
+    "url": "/f-cars/static/media/corolla7.be5e161a.jpg"
+  },
+  {
+    "revision": "dd6e119588ab0a88bf6c008fe6dd6639",
+    "url": "/f-cars/static/media/prius1.dd6e1195.jpg"
+  },
+  {
+    "revision": "3e85a0f9c316b749ae92c0ff79b8e145",
+    "url": "/f-cars/static/media/prius2.3e85a0f9.jpg"
+  },
+  {
+    "revision": "73617f10fc2c527a571670e54fabf12a",
+    "url": "/f-cars/static/media/prius3.73617f10.jpg"
+  },
+  {
+    "revision": "e9219ea717195abba4c2734b021e8f60",
+    "url": "/f-cars/static/media/prius4.e9219ea7.jpg"
+  },
+  {
+    "revision": "c1f0c169ab605c29e111f70d21e4f2db",
+    "url": "/f-cars/static/media/prius5.c1f0c169.png"
+  },
+  {
+    "revision": "2e28768af2b8711c8aea12b7fa14a5b0",
+    "url": "/f-cars/static/media/prius6.2e28768a.jpg"
+  },
+  {
+    "revision": "a8efe48526845c8ac9d7a292258bb5f9",
+    "url": "/f-cars/static/media/prius7.a8efe485.jpg"
+  },
+  {
+    "revision": "a8dded44adc86c49ea96550722b7716d",
+    "url": "/f-cars/static/media/prado1.a8dded44.jpg"
+  },
+  {
+    "revision": "c74e181ebc7df92b4d3f400cfbb9a8ce",
+    "url": "/f-cars/static/media/prado2.c74e181e.jpg"
+  },
+  {
+    "revision": "1ed2fd0f59d3f41fa92b767c02c4d371",
+    "url": "/f-cars/static/media/prado3.1ed2fd0f.jpg"
+  },
+  {
+    "revision": "7c8acad1833d802c4e3a46e74d81c03f",
+    "url": "/f-cars/static/media/prado4.7c8acad1.jpg"
+  },
+  {
+    "revision": "45f7b172da4acba2e086d6d5ae8c3e81",
+    "url": "/f-cars/static/media/prado5.45f7b172.jpg"
+  },
+  {
+    "revision": "b826bf31fddc33a508eb3691eddda653",
+    "url": "/f-cars/static/media/prado6.b826bf31.jpg"
+  },
+  {
+    "revision": "8b9e748e67441b1b6ac26db2e15d10cf",
+    "url": "/f-cars/static/media/prado7.8b9e748e.jpg"
+  },
+  {
+    "revision": "20f67defce368dfa51a614a907267481",
+    "url": "/f-cars/static/media/s1.20f67def.jpg"
+  },
+  {
+    "revision": "92524195521ee85fabf9133a26118b56",
+    "url": "/f-cars/static/media/s2.92524195.jpg"
+  },
+  {
+    "revision": "623bb3df0b6a21a2bc957fcbfb5af376",
+    "url": "/f-cars/static/media/s3.623bb3df.jpg"
+  },
+  {
+    "revision": "5d5e6275b4ae96e6ade6345329ac840e",
+    "url": "/f-cars/static/media/bill.5d5e6275.jpg"
+  },
+  {
+    "revision": "d0d9af14778e47ce292e716bd45712e9",
+    "url": "/f-cars/static/media/stev.d0d9af14.png"
+  },
+  {
+    "revision": "6924fe5d975e7c6dcae9b7f8c4ef564b",
+    "url": "/f-cars/static/media/logo.6924fe5d.jpg"
+  },
+  {
+    "revision": "67cafeed17d105519ac31296d993b9d5",
+    "url": "/f-cars/static/media/g1.67cafeed.jpg"
+  },
+  {
+    "revision": "a11234c093d09da20287c2bb8ffa7c6b",
+    "url": "/f-cars/static/media/g2.a11234c0.jpg"
+  },
+  {
+    "revision": "f0a21942b866dff236010b73371b74a7",
+    "url": "/f-cars/static/media/g3.f0a21942.jpg"
+  },
+  {
+    "revision": "a42744d1b934d80c1031d608061b9dc9",
+    "url": "/f-cars/static/media/g4.a42744d1.jpg"
+  },
+  {
+    "revision": "16a0738f99a0fe5add2d0b9e8ff833d7",
+    "url": "/f-cars/static/media/g5.16a0738f.jpg"
+  },
+  {
+    "revision": "b907676841972d0a3670cffd8c5f0067",
+    "url": "/f-cars/static/media/g6.b9076768.jpg"
+  },
+  {
+    "revision": "f41db78b5bbccd82cc406e2473b8cc35",
+    "url": "/f-cars/static/media/g7.f41db78b.jpg"
+  },
+  {
+    "revision": "35b4f645695b634ed00d0c0c448bd257",
+    "url": "/f-cars/static/media/g8.35b4f645.jpg"
+  },
+  {
+    "revision": "f85801477d84f7704b56928e24a69d2c",
+    "url": "/f-cars/static/media/g9.f8580147.jpg"
+  },
+  {
+    "revision": "a4eaff058c83b87743e974c940e17419",
+    "url": "/f-cars/static/media/g10.a4eaff05.jpg"
+  },
+  {
+    "revision": "38c50b0a9d4f79139ae25709af6c78ec",
+    "url": "/f-cars/static/media/g11.38c50b0a.jpg"
+  },
+  {
+    "revision": "cb6bc428aa463da7401aefd643a8986c",
+    "url": "/f-cars/static/media/g12.cb6bc428.jpg"
+  },
+  {
+    "revision": "2044377513624d15792eb034da8bbc0a",
+    "url": "/f-cars/static/media/g13.20443775.jpg"
+  },
+  {
+    "revision": "899fdf3cdc8fac2496400b90d21e4892",
+    "url": "/f-cars/static/media/g14.899fdf3c.jpg"
+  },
+  {
+    "revision": "0006158900912bf231e7343605ffe400",
+    "url": "/f-cars/static/media/g15.00061589.jpg"
+  },
+  {
+    "revision": "031268cc244abe06712ff42a6d97a1a3",
+    "url": "/f-cars/static/media/g17.031268cc.jpg"
+  },
+  {
+    "revision": "7bf341d0694d673d8c3abf7b6671ade0",
+    "url": "/f-cars/static/media/g18.7bf341d0.jpg"
+  },
+  {
+    "revision": "6bdb4d0fe5ccded09b2d83ea03f07f62",
+    "url": "/f-cars/static/media/corolla.6bdb4d0f.jpg"
+  },
+  {
+    "revision": "77ee7e9a1566f351f5620ae04cd28405",
+    "url": "/f-cars/static/media/prado.77ee7e9a.jpg"
+  },
+  {
+    "revision": "db871c1ac40088f2dc99e3b334fcd6aa",
+    "url": "/f-cars/static/media/mustang.db871c1a.jpg"
+  },
+  {
+    "revision": "89cdfee47018aacb28d33a527b060c37",
+    "url": "/f-cars/static/media/prius.89cdfee4.jpg"
+  },
+  {
+    "revision": "e3520bed1a2745f4709ce5a0b1dd39fc",
+    "url": "/f-cars/static/media/mercedes.e3520bed.jpg"
+  },
+  {
+    "revision": "7bfbb6269c83f7b764de7f57c43c6443",
+    "url": "/f-cars/static/media/toyota.7bfbb626.jpg"
+  },
+  {
+    "revision": "35919af55deb7a62be91d28621c78cc9",
+    "url": "/f-cars/static/media/owner.35919af5.jpg"
+  },
+  {
+    "revision": "ab94e4a14ec81d615ab8a978ea3e6368",
+    "url": "/f-cars/static/media/manager.ab94e4a1.jpg"
+  },
+  {
+    "revision": "a673da2cd7f3bf3415d01cef8dc098a5",
+    "url": "/f-cars/static/media/ceo.a673da2c.jpg"
+  },
+  {
+    "revision": "83b6785ae441a1df6203f3c2b153effd",
+    "url": "/f-cars/static/media/cfo.83b6785a.jpg"
+  },
+  {
+    "revision": "aab2cc4182fceb633dd90dc871bc4295",
+    "url": "/f-cars/static/media/logo.aab2cc41.png"
+  },
+  {
+    "revision": "a53fcad78d7e78b55fa279d952fd712d",
+    "url": "/f-cars/static/media/mustang1.a53fcad7.png"
+  },
+  {
+    "revision": "ec941a179fd658678f543c776ff967fb",
+    "url": "/f-cars/static/media/mustang2.ec941a17.png"
+  },
+  {
+    "revision": "a59a58b416af46e1f91e2bc143ddd566",
+    "url": "/f-cars/static/media/mustang3.a59a58b4.jpg"
+  },
+  {
+    "revision": "6da2b1e35855582d3da36d38e1cafaad",
+    "url": "/f-cars/static/media/mustang4.6da2b1e3.png"
+  },
+  {
+    "revision": "03555c50b84ad84a4609f927bd52e8a6",
+    "url": "/f-cars/static/media/mustang5.03555c50.png"
+  },
+  {
+    "revision": "2e0d9422462b2c6523dec67c18b08511",
+    "url": "/f-cars/static/media/mustang6.2e0d9422.jpg"
+  },
+  {
+    "revision": "a18623105d4e1061f60665872ef52caa",
+    "url": "/f-cars/static/media/mustang7.a1862310.jpg"
+  },
+  {
+    "revision": "f889ad09c1ca940ff8f37ef0e0f769e4",
+    "url": "/f-cars/static/media/mercedes1.f889ad09.jpg"
+  },
+  {
+    "revision": "623602d045c1a692abdb1f457a107bbd",
+    "url": "/f-cars/static/media/mercedes2.623602d0.jpg"
+  },
+  {
+    "revision": "1d76f6ea2f136ec68482d524d3ba734b",
+    "url": "/f-cars/static/media/mercedes3.1d76f6ea.jpg"
+  },
+  {
+    "revision": "70fffb7ad0da8398f40689d59d8833c1",
+    "url": "/f-cars/static/media/mercedes4.70fffb7a.jpg"
+  },
+  {
+    "revision": "76a236f1336e2967696e39cc7a7f0f09",
+    "url": "/f-cars/static/media/mercedes5.76a236f1.jpg"
+  },
+  {
+    "revision": "e58357a71e3ea88f20fe842236fd471f",
+    "url": "/f-cars/static/media/mercedes6.e58357a7.jpg"
+  },
+  {
+    "revision": "c8a6d63fc0c97245cdab6eedd887b287",
+    "url": "/f-cars/static/media/mercedes7.c8a6d63f.jpg"
+  },
+  {
+    "revision": "5253632c649e459f26606b809764422b",
+    "url": "/f-cars/static/media/toyota1.5253632c.jpg"
+  },
+  {
+    "revision": "465633bd6a820a0462d850cd43bd1270",
+    "url": "/f-cars/static/media/toyota2.465633bd.png"
+  },
+  {
+    "revision": "c31de4be4b1bf550644dac42f471118d",
+    "url": "/f-cars/index.html"
+  }
+];
